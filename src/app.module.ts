cd1baff3f133c311ba394675ed7a2fdb37ebdad8
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-04-30 17:55:46 -0400

    wip: added typeorm and config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,34 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ItemsModule } from './items/items.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
       playground: false,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+
     ItemsModule,
   ],
   controllers: [],
